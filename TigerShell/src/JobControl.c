2fb438d8dbc7df3e677f779ed60a9c8dcfc04513
Jun <zhouj@lafayette.edu>
2020-02-24 22:36:30 -0500

    starting man

--- TigerShell/src/JobControl.c
+++ TigerShell/src/JobControl.c
@@ -1,13 +1,61 @@
 #include "JobControl.h"
+#include <stdio.h>
 
 int nextJid = 1;
-jobData jobList[50] = {NULL};
+jobData jobList[50];
 
 
-int addJob(int jidInput, int pidInput, char* commandInput){
+
+int printJob(jobData jobDataInput){
+	printf("%5d",jobDataInput.jid);
+	printf("%5d",jobDataInput.pid);
+	printf("%7d",jobDataInput.state);
+	printf("%7s/n",jobDataInput.commandName);
+	return 0;
 
 }
 
-int findAvailable(){
-	for()
+int jobs(){
+	printf("%5s","JID");
+	printf("%5s","PID");
+	printf("%7s","status");
+	printf("%7s/n","command");
+	for (int i = 0; i < 50; ++i){
+		if(jobList[i].jid !=0){
+			printJob(jobList[i]);
+		}
+    		
+  	} 
+	return 0;
+
 }
+
+int printJsum(jobData jobDataInput){
+	printf("%5d",jobDataInput.pid);
+	printf("%5d",jobDataInput.state);
+	printf("%10f",(double)(clock()-jobDataInput.startTime));
+	printf("%10d",jobDataInput.minFault);
+	printf("%10d",jobDataInput.maxFault);
+	printf("%7s/n",jobDataInput.commandName);
+	return 0;
+
+}
+
+int jsum(){
+	printf("%5s","PID");
+	printf("%5s","status"); 
+	printf("%10s","ElapsedTime");
+	printf("%10s","Min Fault");
+	printf("%10s","Max Fault");
+	printf("%7s/n","commandName");
+	for (int i = 0; i < 50; ++i){
+		if(jobList[i].jid !=0){
+			printJsum(jobList[i]);
+		}
+    		
+  	} 
+	return 0;
+
+}
+
+
